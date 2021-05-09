5187d9f156f3e0040614502bb1baf07bbbfcb200
Martin René Sørensen <git@sicet7.com>
2021-05-09 11:15:04 +0200

    Changed volume mounts. Changed config paths. Made init not fail if ran multiple times.

--- init.sh
+++ init.sh
@@ -2,15 +2,17 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )";
 
-if [ ! -f "$SCRIPTPATH/src/traefik-data/acme-prod.json" ]; then
-  echo "{}" > "$SCRIPTPATH/src/traefik-data/acme-prod.json"
-  sudo chmod 0600 "$SCRIPTPATH/src/traefik-data/acme-prod.json"
+if [ ! -f "$SCRIPTPATH/config/traefik/acme-prod.json" ]; then
+  echo "{}" > "$SCRIPTPATH/config/traefik/acme-prod.json"
+  sudo chmod 0600 "$SCRIPTPATH/config/traefik/acme-prod.json"
 fi
 
-if [ ! -f "$SCRIPTPATH/src/traefik-data/acme-staging.json" ]; then
-  echo "{}" > "$SCRIPTPATH/src/traefik-data/acme-staging.json"
-  sudo chmod 0600 "$SCRIPTPATH/src/traefik-data/acme-staging.json"
+if [ ! -f "$SCRIPTPATH/config/traefik/acme-staging.json" ]; then
+  echo "{}" > "$SCRIPTPATH/config/traefik/acme-staging.json"
+  sudo chmod 0600 "$SCRIPTPATH/config/traefik/acme-staging.json"
 fi
 
-docker network create proxy
-docker network create database
+[ ! "$(docker network ls | grep proxy)" ] && docker network create proxy
+[ ! "$(docker network ls | grep database)" ] && docker network create database
+
+echo "Server is Successfully initialized."
